f3fe6d34fadfb0810fbd69dc86a1abfa1b77cf1a
Edwin <edwinlyar@gmail.com>
2025-10-29 21:31:44 +1100

    Push 5

--- Main.py
+++ Main.py
@@ -1,23 +1,23 @@
 import datacleaner
 import dataimporter
+import sys 
 
 def main():
     print('Initialising...') 
     print("Starting Data Cleaner...")
+
     success = datacleaner.main()
 
     if not success:
-        print("Data Cleaner failed")
-        exit
-   
-print("Starting Data Importer...")
-try:
-    dataimporter.main()
-except Exception as e:
-    print(f"Data Importer failed: {e}")
- 
+        print("Data Cleaner failed. Exiting program.")
+        sys.exit()  
+
+    print("Starting Data Importer...")
+    try:
+        dataimporter.main()
+    except Exception as e:
+        print(f"Data Importer failed: {e}")
 
 if __name__ == "__main__": 
     main()
     input('Press Enter to exit...')
-
